f475c472e614ccd23463ec14ba918d45125aca7b
Harsh-coder320 <harshmanojdev@gmail.com>
2026-05-16 01:24:48 +0530

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -14,7 +14,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://gravityflow-task-manager-gny9.vercel.app",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
